8cc0c5a684af5a468b0bfee664d27b313aaaaff0
yanarkhan <rianarkhan@gmail.com>
2024-10-07 18:39:21 +0700

    Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props;
+  return (
+    <button
+      class={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="bg-blue-600 flex justify-center min-h-screen items-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex gap-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
