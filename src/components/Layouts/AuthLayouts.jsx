b7a03d255f1a41dbd3ce16fb3a0036b744b83b56
yanarkhan <rianarkhan@gmail.com>
2024-10-09 12:39:51 +0700

    Conditional Rendering

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AuthLayouts = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
@@ -10,9 +11,59 @@ const AuthLayouts = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+        {/* <p className="text-sm mt-5 text-center">
+          {type === "login"
+            ? "Don't have an account? "
+            : "Already have an account? "}
+
+          {type === "login" ? (
+            <Link to="/register" className="font-bold text-blue-600">
+              Register
+            </Link>
+          ) : (
+            <Link to="/login" className="font-bold text-blue-600">
+              Login
+            </Link>
+          )}
+
+          {type === "login" && (
+            <Link to="/register" className="font-bold text-blue-600">
+              Register
+            </Link>
+          )}
+          {type === "register" && (
+            <Link to="/login" className="font-bold text-blue-600">
+              Login
+            </Link>
+          )}
+        </p> */}
+
+        <Navigation type={type} />
       </div>
     </div>
   );
+};
+
+const Navigation = ({ type }) => {
+  if (type === "login") {
+    return (
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          Register
+        </Link>
+      </p>
+    );
+  } else {
+    return (
+      <p className="text-sm mt-5 text-center">
+        Already have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Login
+        </Link>
+      </p>
+    );
+  }
 };
 
 export default AuthLayouts;
